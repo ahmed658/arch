cca7ae082239224efaed763b3b3030f7711e7fe2
MahmoudMosalam <89727387+MahmoudMosalam@users.noreply.github.com>
2025-03-09 13:17:12 +0200

    Update install_arch.sh

--- install_arch.sh
+++ install_arch.sh
@@ -22,20 +22,33 @@ if [ ! -f /etc/hostid ]; then
   sudo zgenhostid
 fi
 
-# Ensure EFI partition is formatted and mounted
+# Disk and partition variables
 DISK=/dev/nvme0n1
 EFI_PART=${DISK}p1
-if ! mount | grep -q '/mnt/efi'; then
-  sudo mkfs.vfat -F 32 -n EFI $EFI_PART
-  sudo mkdir -p /mnt/efi
-  sudo mount $EFI_PART /mnt/efi
-fi
+ZFS_PART=${DISK}p2
+
+# Partition the disk
+parted --script $DISK mklabel gpt
+parted --script $DISK mkpart primary fat32 1MiB 2049MiB
+parted --script $DISK set 1 boot on
+parted --script $DISK mkpart primary 2049MiB 100GiB
+
+# Format the EFI partition
+sudo mkfs.vfat -F 32 -n EFI $EFI_PART
+
+# Create ZFS pool
+sudo zpool create -f -O mountpoint=none zroot $ZFS_PART
+sudo zfs create -o mountpoint=legacy zroot/ROOT
+sudo zfs create -o mountpoint=legacy zroot/ROOT/default
+sudo zpool set bootfs=zroot/ROOT/default zroot
+
+# Mount the partitions
+sudo mount -t zfs zroot/ROOT/default /mnt
+sudo mkdir -p /mnt/boot
+sudo mount $EFI_PART /mnt/boot
 
 # Install base system and tools
-sudo pacstrap /mnt base linux-lts linux-firmware linux-lts-headers wget nano vim efibootmgr
-
-# Install ZFS support
-sudo pacstrap /mnt zfs-dkms
+sudo pacstrap /mnt base linux-lts linux-firmware linux-lts-headers wget nano vim efibootmgr zfs-dkms
 
 # Copy necessary files
 sudo cp /etc/hostid /mnt/etc
@@ -44,8 +57,8 @@ sudo mkdir -p /mnt/etc/zfs
 sudo cp /etc/pacman.conf /mnt/etc/pacman.conf
 
 # Generate and edit fstab
-sudo genfstab /mnt > /mnt/etc/fstab
-sudo sed -i '/^\/mnt\/efi/!d' /mnt/etc/fstab
+sudo genfstab -U /mnt > /mnt/etc/fstab
+sudo sed -i '/^\/mnt\/boot/!d' /mnt/etc/fstab
 
 # Chroot into the new system
 arch-chroot /mnt <<EOF
@@ -75,23 +88,23 @@ sed -i 's/^HOOKS.*/HOOKS=(base udev autodetect modconf block keyboard zfs filesy
 mkinitcpio -P
 
 # Set root password
-echo "AcerPowerF#1" | passwd --stdin
+echo "root:AcerPowerF#1" | chpasswd
 
 # Set ZFS configurations
 zpool set cachefile=/etc/zfs/zpool.cache zroot
-zpool set bootfs=zroot/ROOT/arch zroot
+zpool set bootfs=zroot/ROOT/default zroot
 systemctl enable zfs-import-cache zfs-import.target zfs-mount zfs-zed zfs.target
 
 # Install ZFS Boot Menu
-mkdir -p /efi/EFI/zbm
-wget https://get.zfsbootmenu.org/latest.EFI -O /efi/EFI/zbm/zfsbootmenu.EFI
+mkdir -p /boot/efi/EFI/zbm
+wget https://get.zfsbootmenu.org/latest.EFI -O /boot/efi/EFI/zbm/zfsbootmenu.EFI
 efibootmgr --disk $DISK --part 1 --create --label "ZFSBootMenu" --loader '\EFI\zbm\zfsbootmenu.EFI' --unicode "spl_hostid=\$(hostid) zbm.timeout=3 zbm.prefer=zroot zbm.import_policy=hostid" --verbose
 zfs set org.zfsbootmenu:commandline="noresume init_on_alloc=0 rw spl.spl_hostid=\$(hostid)" zroot/ROOT
 
 EOF
 
 # Unmount and export ZFS pool
-sudo umount /mnt/efi
+sudo umount /mnt/boot
 sudo zpool export zroot
 
 echo "Installation complete! You can now reboot."
